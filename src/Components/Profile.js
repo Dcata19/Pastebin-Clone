9eaee71591bf81423c2a2a7ab1b7d48eaab7f165
David Catalin <106652441+Dcata19@users.noreply.github.com>
2022-08-18 16:32:05 +0300

    Add files via upload

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,29 +1,24 @@
 import { collection, query, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { db, auth } from "../firebase.js";
 
 export default function Profile() {
 
     const [pastes, setPastes] = useState([]);
 
-    function show() {
-        auth.onAuthStateChanged(user => {
-            if (user) {
-                const q = query(collection(db, user.email));
-                onSnapshot(q, (querySnapshot) => {
-                    const pastes = [];
-                    querySnapshot.forEach((doc) => {
-                        pastes.push(doc.data());
-                    });
-                    setPastes(pastes);
-                });
-            }
-        })
-    }
 
-    useEffect(() => {
-        show();
-    }, [])
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            const q = query(collection(db, user.email));
+            onSnapshot(q, (querySnapshot) => {
+                const pastes = [];
+                querySnapshot.forEach((doc) => {
+                    pastes.push(doc.data());
+                });
+                setPastes(pastes);
+            });
+        }
+    })
 
     const list = pastes.map((paste, index) =>
         <li className="list-group-item bg-dark text-white" key={index}>
